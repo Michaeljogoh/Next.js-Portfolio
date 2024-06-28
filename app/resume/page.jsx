23dbcc46fd481abc2ee8125ab10c182e48f8a281
unknown <michaeljogoh@gmail.com>
2024-06-28 11:50:27 +0100

    Update contact resume pages

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -194,7 +194,7 @@ const Resume = () => {
     return (
         <motion.div 
         initial={{ opacity: 0}} 
-        animate={{opacity:1, transition:{ delay:2.4, duration: 0.4, ease:"easeIn" }}}
+        animate={{opacity:1, transition:{ delay:1.5, duration: 0.3, ease:"easeIn" }}}
         className="min-h-[80vh] flex items-center justify-center py-12 xl:py-0"
         >
          <div className="container mx-auto">
--- app/work/page.jsx
+++ app/work/page.jsx
@@ -96,7 +96,7 @@ const Work = () => {
     }
 
     return (
-        <motion.section initial={{opacity:0}} animate={{opacity:1, transition:{delay:2.4, duration: 0.4, ease:"easeIn"}}} 
+        <motion.section initial={{opacity:0}} animate={{opacity:1, transition:{delay:1.5, duration: 0.3, ease:"easeIn"}}} 
         className="min-h-[80vh] flex flex-col justify-center py-12 xl:px-0" 
          >
            <div className="container mx-auto">
